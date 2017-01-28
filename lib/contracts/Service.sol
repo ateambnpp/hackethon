946be82a666272f371f1285bbcd4260c52d13e1a
vincent <vincent.juge@gmail.com>
2017-01-28 17:24:04 +0100

    qsdsq

--- lib/contracts/Service.sol
+++ lib/contracts/Service.sol
@@ -1,83 +1,35 @@
 pragma solidity ^0.4.2;
 
-import "Actions.sol";
-
-
-// several services for several devices can be attached
-// each inputs / outputs availabilities are coded as a uint
 contract Service {
 
-    uint[] inputs;
-    uint[] outputs;
+    mapping (string => bool) mInputs;
+    mapping (string => bool) mOutputs;
 
-    enum serviceInputType { Toggle, Text }
+    string name;
+    string id;
 
-    //function getInputType() returns () {}
-
-    function getInputIndex(uint _index) public returns(uint){
-        return inputs[_index];
+    function enableInput(string _actionInput) public {
+        mInputs[_actionInput] = true;
     }
 
-    function getInputSize() public returns(uint){
-        return inputs.length;
+    function disabelInput(string _actionInput) public {
+        mInputs[_actionInput] = false;
     }
 
-    function removeInput(uint _index) notOutOfRange(_index, inputs) {
-
+    function enableOutput(string _actionOutput) public {
+        mOutputs[_actionOutput] = true;
     }
 
-    function getOutputIndex(uint _index) notOutOfRange(_index, outputs) public returns(uint){
-        return outputs[_index];
-    }
-
-    function removeInputIndex(uint _index) notOutOfRange(_index, inputs) {
-        inputs[_index] = inputs[inputs.length - 1];
-        delete inputs[inputs.length-1];
-    }
-
-    function removeOutputIndex(uint _index) notOutOfRange(_index, outputs){
-        outputs[_index] = outputs[outputs.length - 1];
-        delete outputs[outputs.length-1];
-    }
-
-    function Service(){}
-
-    function getServiceType() public returns (uint){}
-
-    modifier notOutOfRange(uint i, uint[] t){
-        if (i < t.length) _;
+    function disabelOutput(string _actionOutput) public {
+        mOutputs[_actionOutput] = false;
     }
 
 }
 
+contract Device is Service {
+    address public owner;
 
-// panic service
-// serviceType for boolean is "1"
-contract ServiceBoolean is Service {
-    bool public state;
-    string public data;
-    string public apicall;
-    uint public serviceType;
-
-    function DeviceBoolean(string _apicall){
-        apicall = _apicall;
-        serviceType = 1;
-    }
-
-    function getServiceType() public returns (uint){
-        return serviceType;
-    }
-
-    function getState() public returns (bool){
-        return state;
-    }
-
-    function getData() public returns (string){
-        return data;
-    }
-
-    function setState(bool _state, string _data) public {
-        state = _state;
-        data = _data;
+    function Device(){
+        owner = msg.sender;
     }
 }
